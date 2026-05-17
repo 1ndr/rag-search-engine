340153c814bce141b022085d89cad704fb737c40
1ndr <archie.jayawantha@gmail.com>
2026-05-17 16:27:41 +0800

    added single text embedding

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,5 +1,5 @@
 import argparse
-from lib.semantic_search import verify_model
+from lib.semantic_search import verify_model, embed_text
 
 
 def main() -> None:
@@ -8,11 +8,18 @@ def main() -> None:
 
     verify_model_parser = subparsers.add_parser("verify", help="Verify that the embedded model is loaded")
 
+    embed_text_parser = subparsers.add_parser("embed_text", help="embed the given text")
+    embed_text_parser.add_argument("text", type=str, help="The text to be embeded")
+
     args = parser.parse_args()
     match args.command:
         case "verify":
             print("Verifying model used for Semantic Search")
             verify_model()
+
+        case "embed_text":
+            print(f'Embedding text: "{args.text}"')
+            embed_text(args.text)
 
         case _:
             parser.print_help()
